cdca02f17dcbe91e6c02ee1c5fd777964125c3cb
kellyshofner <kellyshofner1128@gmail.com>
2025-09-09 14:32:01 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "3rem", fontFamily: "Arial" }}>
+      <h1>Kelly Investigates</h1>
+      <p>This is the starting point of the investigation website.</p>
+    </main>
+  )
+}
